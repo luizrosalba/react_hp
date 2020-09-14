5a83d3da9e4b6f0bf81fc85bb8c034ecbc67611b
Luiz <luizrosalba@gmail.com>
2020-09-14 12:39:57 -0300

    editando programacao

--- hp-app/src/pages/Programacao/index.js
+++ hp-app/src/pages/Programacao/index.js
@@ -5,9 +5,45 @@ const Programacao = () => {
     <header className="App-body">
       <div className="container">
       <div className="title_page"> Conheça Meu GitHub </div>
-        Diversos repositórios que desenvolvi durante minha formação.
-      </div>
+        Diversos repositórios que desenvolvi durante minha formação. 
+      <div className="title_page">  Projetos Interessantes</div>
+            <p className="links">
+              <a href = "https://github.com/luizrosalba/DadosApiCriptomoedas"                               target= "_blank"  rel="noopener noreferrer" > Requisitando dados de uma API de criptomoedas    </a>  
+              <a href = "https://github.com/luizrosalba/ChatbotFitTelegramJavaScript-NodeJS"                target= "_blank"  rel="noopener noreferrer" > ChatbotFit no Telegram com JavaScript e Node.js    </a>  
+              <a href = "https://github.com/luizrosalba/FilasNoNodejs"                                      target= "_blank" rel="noopener noreferrer" > Tratamento de Filas no Node.js   </a>  
+              <a href = "https://github.com/luizrosalba/RecriandopaginaInstagram"                           target= "_blank" rel="noopener noreferrer" > Recriando a página do Instagram usando CSS   </a> 
+              <a href = "https://github.com/luizrosalba/netflix-clone-master"                               target= "_blank" rel="noopener noreferrer" > Recriando a página do Netflix.   </a> 
+              <a href = "https://github.com/luizrosalba/Bootstrap"                                          target= "_blank" rel="noopener noreferrer" > Construindo páginas para internet com Bootstrap   </a> 
+              <a href = "https://github.com/luizrosalba/Arduino"                                            target= "_blank" rel="noopener noreferrer" > Alguns projetos interessantes para Arduino   </a> 
+              <a href = "https://github.com/luizrosalba/Tarefasem-background-utilizando-Nodejs-e-Redis"     target= "_blank" rel="noopener noreferrer" > Tarefas em Background usando Node.js   </a> 
+              <a href = "https://github.com/luizrosalba/Matematica_Basica_Android_Visual_Studio"            target= "_blank" rel="noopener noreferrer" > Matemática Básica Com Oxyplot e Android Studio   </a> 
+              <a href = "https://github.com/luizrosalba/MetodosNumericosCpp"                                target= "_blank" rel="noopener noreferrer" > Uma biblioteca em C++ para Métodos Numéricos   </a> 
+            </p>
+     
 
+      <div className="title_page"> Meus Estudos </div>
+        Repositórios que utilizo para armazenar meus estudos.
+
+          <p className="links">  
+            <a href = "https://github.com/luizrosalba/aprendendo_git"                                                       target= "_blank"  rel="noopener noreferrer" > Aprendendo Git   </a>  
+                    <a href = "https://github.com/luizrosalba/js_free_code_camp"                                            target= "_blank"  rel="noopener noreferrer" > Estudos em Javascript   </a>  
+                    <a href = "https://github.com/luizrosalba/FundamentosdeArquiteturadeSistemas"                           target= "_blank"  rel="noopener noreferrer" > Fundamentos de Arquitetura de Sistemas   </a>  
+                    <a href = "https://github.com/luizrosalba/Introducao_ao_ES6"                                            target= "_blank"  rel="noopener noreferrer" > Introdução ao ES6   </a>  
+                    <a href = "https://github.com/luizrosalba/javascript_es6"                                               target= "_blank"  rel="noopener noreferrer" > Javascript com ES6    </a>  
+                    <a href = "https://github.com/luizrosalba/DesenvolvimentoAvancadoComJSES6"                              target= "_blank"  rel="noopener noreferrer" > Desenvolvimento avançado em Js com ES6   </a>  
+                    <a href = "https://github.com/luizrosalba/ArquiteturainternanoNodeefilas.md"                            target= "_blank"  rel="noopener noreferrer" > Arquitetura interna do Node.js  </a>  
+                    <a href = "https://github.com/luizrosalba/node-hapijs"                                                  target= "_blank"  rel="noopener noreferrer" > Node com hapijs   </a>  
+                    <a href = "https://github.com/luizrosalba/node-websockets"                                              target= "_blank"  rel="noopener noreferrer" > Node com websockets  </a>  
+                    <a href = "https://github.com/luizrosalba/node-mongoose"                                                target= "_blank"  rel="noopener noreferrer" > Node com Mongoose  </a>  
+                    <a href = "https://github.com/luizrosalba/node-tdd"                                                     target= "_blank"  rel="noopener noreferrer" > Node TDD - Test Driven Development  </a>  
+                    <a href = "https://github.com/luizrosalba/Introdu-o-ao-domain-driven-design-e-padr-es-de-arquitetura"   target= "_blank"  rel="noopener noreferrer" > Introdução ao domain driven design (DDD) e padrões de Arquitetura - Programação Ágil  </a>  
+                    <a href = "https://github.com/luizrosalba/SCRUM"                                                        target= "_blank"  rel="noopener noreferrer" > O Processo SCRUM  </a>  
+                    <a href = "https://github.com/luizrosalba/Arquitetura-de-Sistemas-Avan-ado"                             target= "_blank"  rel="noopener noreferrer" > Arquitetura de Sistemas Avançados </a>   
+                    <a href = "https://github.com/luizrosalba/asyncawait"                                                   target= "_blank"  rel="noopener noreferrer" > Como funcionam os métodos Async e Await  </a>                        
+                    <a href = "https://github.com/luizrosalba/EntendendoPromisesdeumavezportodas "                          target= "_blank"  rel="noopener noreferrer" > Ententendo Promisses </a>                        
+                   
+           </p>
+           </div>
     </header>
   )
 }
